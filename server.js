419318a3d3508e575be71b2ea396539fa2b594d2
Joshua Lee <joshualee@iconfigure.org>
2016-12-28 16:54:52 -1000

    server now able to write to client

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const net = require('net');
+const EventEmitter = require('events');
+const util = require('util');
 const fs = require('fs');
 
-var server = net.createServer((socket) => {
+let emitEvent = new EventEmitter();
+
+// function call_back() {
+//   console.log('Call back has been called');
+// }
+
+let server = net.createServer((socket) => {
   // socket.end('goodbye\n');
   socket.setEncoding('utf8');
-  socket.write('hello\n');
-  socket.on('data', (socket) => {
-    let recieveData = fs.createWriteStream()
+  socket.write('connected');
+  process.stdin.pipe(socket);
+  // socket.on('pipe', function(readme) {
+  //     console.log('i have been seen')
+  // })
+  // socket.on('event', (chunk) => {
+  //  console.log(call_back);
 
-  })
+  // });
+
+
+    // (socket) => {
+    // console.log(`chunk from client ${socket}`)
+  // })
 
 })
-// .on('error', (err) => {
-//   throw err;
-// })
 
 server.listen(6969, '0.0.0.0', () => {
   console.log('opened server on ', server.address())
