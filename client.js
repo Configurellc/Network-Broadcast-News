8d0bff96dd3d465b7f7bb030da1c24663df5675a
Joshua Lee <joshualee@iconfigure.org>
2016-12-28 16:54:20 -1000

    Server is now able to write to the client

--- client.js
+++ client.js
@@ -1,11 +1,16 @@
 const net = require('net');
+const EventEmitter = require('events');
+const util = require('util');
+const fs = require('fs');
 
 const server = net.createConnection(6969, () => {
   console.log('connected');
-  server.write('world\n');
+  // server.write('world\n');
+  process.stdin.pipe(server);
 })
 
-// client.on('data', (data) => {
 
+server.on('data', (data) => {
+  console.log(`Server Broadcast : ${data}`)
 
-// })
+})
